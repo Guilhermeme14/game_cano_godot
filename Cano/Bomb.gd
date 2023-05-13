98ed5fe40eb59c2973d710cf5d07ab6cff399dde
Guilherme Fernandes <guifernandes111@gmail.com>
2023-05-12 23:59:56 -0300

    Add files via upload

---
+++ Cano/Bomb.gd
@@ -0,0 +1,18 @@
+extends KinematicBody2D
+
+var speed = 200
+
+func _ready():
+	$SoundBomb.play()
+
+func _process(delta):
+	var info = move_and_collide(Vector2(0, -speed) * delta)
+	if info:
+		var obj = info.get_collider()
+		if 'Enemy' in obj.name:
+			obj.queue_free()
+			queue_free()
+			get_parent().new_explosion(obj.position)
+			get_parent().change_score(1)
+	if position.y < 0:
+		queue_free()
